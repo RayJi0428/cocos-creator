7a65ce40293fb0692ca063c7316fbbfc8a0fb412
RayJi <kaikai0428@gmail.com>
2020-09-30 16:09:05 +0800

    相關路徑調整

--- protobuf/tools/compile-proto/wrap-pbjs.js
+++ protobuf/tools/compile-proto/wrap-pbjs.js
@@ -1,7 +1,7 @@
-(window || global).awesome = (function($protobuf) {
+(window || global).test = (function($protobuf) {
     "use strict";
 
     $OUTPUT;
 
     return $root;
-})(protobuf).awesome;
+})(protobuf).test;
